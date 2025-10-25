f82971567b6a06d18ca6ba44f31289e2d7b13ba3
rit229 <malikritu845@gmail.com>
2025-10-26 05:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+const strongElements = document.querySelectorAll('strong');
+        strongElements.forEach(function(element) {
+            element.style.color = 'rgb(0, 128, 0)'; // Change to green
+        });
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+const strongElements = document.querySelectorAll('strong');
+        strongElements.forEach(function(element) {
+            element.style.color = 'rgb(0, 0, 0)'; // Change back to black
+        });
     
 }
